d1990e1463e3022d520b77d41f8589345c508086
Jiyu Guo <ddzsw2020@gmail.com>
2024-08-08 23:49:24 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter02/ChangeChar.java
@@ -0,0 +1,32 @@
+package com.iamhotcenplotz.www.chapter02;
+
+public class ChangeChar {
+    public static void Exercise() {
+        System.out.println("+++++++++++++++++++++++++++++");
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+        System.out.println("+++++++++++++++++++++++++++++");
+    }
+
+    public static void main(String[] args) {
+        // 制表位
+        System.out.println("北京\t天津\t上海\t");
+        // 换行符
+        System.out.println("Jack\nSmith\nMarry");
+        // 一个\
+        System.out.println("Hello\\Ohio");
+        System.out.println("Hello\\\\Ohio");
+        // 一个"
+        System.out.println("老韩说:\"要好好学习,有前途\"");
+        // 一个'
+        System.out.println("老韩说:\'要好好学习,有前途\'");
+        // 一个回车
+        System.out.println("DerHotcenplotz\rShenzhen"); // output: Shenzhen
+        System.out.println("DerHotcenplotz\r\nShenzhen");
+
+        Exercise();
+
+
+    }
+
+}
+
---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter02/Comment03.java
@@ -0,0 +1,4 @@
+package com.iamhotcenplotz.www.chapter02;
+
+public class Comment03 {
+}
