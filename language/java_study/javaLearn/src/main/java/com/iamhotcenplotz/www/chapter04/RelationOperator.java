5a83d55ff7072289c2a943f9588e6c94e0c28d3a
Jiyu Guo <ddzsw2020@gmail.com>
2024-08-13 23:41:17 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter04/RelationOperator.java
@@ -0,0 +1,16 @@
+package com.iamhotcenplotz.www.chapter04;
+
+public class RelationOperator {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        boolean flag = a > b;
+        System.out.println("flat = " + flag);
+    }
+}
