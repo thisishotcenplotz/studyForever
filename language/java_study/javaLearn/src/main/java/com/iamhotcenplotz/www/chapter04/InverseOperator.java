674d36a1f56b93628ad67c9cac5cc4e2fbbe7a79
Jiyu Guo <ddzsw2020@gmail.com>
2024-08-14 23:37:08 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter04/InverseOperator.java
@@ -0,0 +1,16 @@
+package com.iamhotcenplotz.www.chapter04;
+
+public class InverseOperator {
+    public static void main(String[] args) {
+        System.out.println(!(60 > 20));
+
+        // case
+        //   when a == true and b == true then true
+        //   when a == false and b == false then true
+        //   else false
+        // end;
+        System.out.println((60 > 20) ^ (30 > 40));
+        System.out.println((60 > 20) ^ (30 < 40));
+        System.out.println((60 < 20) ^ (30 < 40));
+    }
+}
