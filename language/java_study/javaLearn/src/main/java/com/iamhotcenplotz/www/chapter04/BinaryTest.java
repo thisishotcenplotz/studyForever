d4577bdb5c3526073599a82383fbc53ffc2d8632
Jiyu Guo <ddzsw2020@gmail.com>
2024-08-16 22:30:44 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter04/BinaryTest.java
@@ -0,0 +1,14 @@
+package com.iamhotcenplotz.www.chapter04;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        int n1 = 0b1010;
+        int n2 = 1010;
+        int n3 = 01010;
+        int n4 = 0x10101;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
+    }
+}
