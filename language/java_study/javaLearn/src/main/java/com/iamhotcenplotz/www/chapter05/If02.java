2a3786d2008be78e222560e316a99ce386f05f3b
Jiyu Guo <ddzsw2020@gmail.com>
2024-08-18 22:26:17 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter05/If02.java
@@ -0,0 +1,19 @@
+package com.iamhotcenplotz.www.chapter05;
+
+import java.util.Scanner;
+
+public class If02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("input your age: ");
+        int age = scanner.nextInt();
+
+        if (age > 20){
+            System.out.println("adult");
+        }else {
+            System.out.println("child");
+        }
+
+    }
+}
