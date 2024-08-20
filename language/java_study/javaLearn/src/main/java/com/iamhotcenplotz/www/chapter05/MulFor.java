60556ed7e49411773d215e3bdd643dee790096fe
Jiyu Guo <ddzsw2020@gmail.com>
2024-08-20 22:27:52 +0800

    a

--- language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter05/MulFor.java
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter05/MulFor.java
@@ -19,24 +19,58 @@ public class MulFor {
     }
 
     public static void exercise03() {
-        // 空心三角形？
-
-        int size = 15;
+        int size = 10;
         for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                if (j <= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
-            for (int j = 0; j < size / 2; j++) {
+    public static void exercise04() {
+        int size = 15;
+
+        for (int i = 0; i < size; i++) {
+            for (int k = 0; k <= size - i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i - 1; j++) {
                 System.out.print("*");
             }
-            for (int k = size / 2 + 1; k < size; k++) {
+            System.out.println();
+        }
+    }
 
+    public static void exercise05() {
+        int size = 15;
+
+        for (int i = 1; i <= size; i++) {
+            for (int k = 1; k <= size - i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i -1 || i == size){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        exercise01();
-        exercise02();
-        exercise03();
+//        exercise01();
+//        exercise02();
+//        exercise03();
+//        exercise04();
+        exercise05();
     }
 }
