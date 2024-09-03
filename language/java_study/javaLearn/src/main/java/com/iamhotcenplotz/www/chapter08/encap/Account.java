2d728a2c6335cfd47713ad5311729bd97e0f5e46
Jiyu Guo <ddzsw2020@gmail.com>
2024-09-03 22:57:14 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter08/encap/Account.java
@@ -0,0 +1,42 @@
+package com.iamhotcenplotz.www.chapter08.encap;
+
+public class Account {
+    private String name;
+    private double balance;
+    private String password;
+
+    public Account() {
+    }
+
+    public Account(String name, double balance, String password) {
+        this.setName(name);
+        this.setBalance(balance);
+        this.setPassword(password);
+    }
+
+    public void setName(String name) {
+        if (name.length() >= 2 && name.length() <= 4) {
+            this.name = name;
+        } else {
+            System.out.println("len must be between 2~4");
+        }
+    }
+
+    public void setBalance(double balance) {
+        if (balance > 20) {
+            this.balance = balance;
+        } else {
+            System.out.println("balance must greater than 20");
+            this.balance = 0.0;
+        }
+    }
+
+    public void setPassword(String password) {
+        if (password.length() >= 6) {
+            this.password = password;
+        } else {
+            System.out.println("detal:000000");
+            this.password = "000000";
+        }
+    }
+}
