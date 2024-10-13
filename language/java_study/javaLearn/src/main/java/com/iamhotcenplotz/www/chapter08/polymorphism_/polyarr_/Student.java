87b21933fa9d989dbb03122e6f023635cce108f8
Jiyu Guo <ddzsw2020@gmail.com>
2024-10-13 13:05:58 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter08/polymorphism_/polyarr_/Student.java
@@ -0,0 +1,27 @@
+package com.iamhotcenplotz.www.chapter08.polymorphism_.polyarr_;
+
+public class Student extends Person {
+    private double score;
+
+    public Student(String name, int age, double score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String say() {
+        return super.say() + " my score is " + this.score;
+    }
+
+    public void study(){
+        System.out.println("student "+ this.getName() + " is studying java...");
+    }
+}
---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter08/polymorphism_/polyarr_/Teacher.java
@@ -0,0 +1,22 @@
+package com.iamhotcenplotz.www.chapter08.polymorphism_.polyarr_;
+
+public class Teacher extends Person {
+    private double salary;
+
+    public Teacher(String name,int age,double salary) {
+        super(name,age);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void teach(){
+        System.out.println("teacher " + this.getName() + " is teaching...");
+    }
+}
