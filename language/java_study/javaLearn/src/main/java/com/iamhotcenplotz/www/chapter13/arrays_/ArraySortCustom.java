68f7d8c7fd3cd45b28827c5a8bb437901ae53183
Jiyu Guo <ddzsw2020@gmail.com>
2024-11-30 18:36:11 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter13/arrays_/ArraySortCustom.java
@@ -0,0 +1,55 @@
+package com.iamhotcenplotz.www.chapter13.arrays_;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author Der Hotcenplotz
+ * @version 1.0
+ */
+public class ArraySortCustom {
+    public static void main(String[] args) {
+        int[] arr = {1,-2,8,0,20};
+//        bubble(arr);
+        bubbleCustomize(arr, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return (int)o1 - (int)o2;
+            }
+        });
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    // bubble sort
+    public static void bubble(int[] arr){
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1 -i; j++) {
+                // 从小到大
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // bubble + custom
+
+    public static void bubbleCustomize(int[] arr, Comparator<Integer> c){
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1 -i; j++) {
+                // 从小到大
+                if(c.compare(arr[j],arr[j+1]) > 0){ // 决定从小到大还是从大到小
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
--- language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter13/arrays_/Arrays_.java
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter13/arrays_/Arrays_.java
@@ -15,6 +15,7 @@ public class Arrays_ {
 
         Integer[] toSort = {1,10,3,20,5,30,7};
 
+        // binary sort
         Arrays.sort(toSort);
 
         Arrays.sort(toSort,new Comparator<Integer>() {
@@ -26,5 +27,16 @@ public class Arrays_ {
         });
 
         System.out.println(Arrays.toString(toSort));
+        Integer[] integers = Arrays.copyOf(toSort, toSort.length);
+        Integer[] integers2 = Arrays.copyOf(toSort, toSort.length + 1);
+
+        System.out.println(Arrays.toString(integers));
+        System.out.println(Arrays.toString(integers2));
+
+        int[] a = {1,2,3};
+        // 用指定的数字，替换列表中的所有元素
+        Arrays.fill(a,123);
+
+        System.out.println(Arrays.toString(a));
     }
 }
