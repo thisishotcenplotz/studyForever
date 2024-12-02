5ace3d752dd622aded39feb46a9d519e67085b87
Jiyu Guo <ddzsw2020@gmail.com>
2024-12-02 23:02:06 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/www/chapter13/Exercise.java
@@ -0,0 +1,36 @@
+package com.iamhotcenplotz.www.chapter13;
+
+/**
+ * @author Der Hotcenplotz
+ * @version 1.0
+ */
+public class Exercise {
+    public static void main(String[] args) {
+        System.out.println(Exercise01.reverse("000 123 000", 3, 7));
+    }
+}
+
+class Exercise01 {
+    public static String reverse(String str, int start, int end) {
+        if(start >= end){
+            throw new RuntimeException("...");
+        }
+
+        if (end > str.length()){
+            throw new RuntimeException("+++");
+        }
+
+
+        char[] charArray = str.toCharArray();
+
+        char temp;
+
+        for (int i = start, j = end; i < j; i++, j--) {
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+        }
+
+        return String.valueOf(charArray);
+    }
+}
