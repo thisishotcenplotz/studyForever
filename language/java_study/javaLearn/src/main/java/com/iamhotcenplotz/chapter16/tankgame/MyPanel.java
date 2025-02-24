724c604bf30a7fee32eb8783abed6c6ec0b94858
Jiyu Guo <ddzsw2020@gmail.com>
2025-02-24 23:58:10 +0800

    a

--- language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/chapter16/tankgame/MyPanel.java
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/chapter16/tankgame/MyPanel.java
@@ -22,4 +22,36 @@ public class MyPanel extends JPanel {
 
         g.fillRect(0,0,1000,750);
     }
+
+    /**
+     * @param x
+     * @param y
+     * @param g graphics
+     * @param direct up down left right
+     * @type type color
+     * */
+    public void drawTank(int x,int y,Graphics g, int direct,int type){
+
+        //  设置颜色
+        switch (type){
+            case 0:
+                g.setColor(Color.cyan);
+                break;
+            case 1:
+                g.setColor(Color.yellow);
+                break;
+        }
+
+
+        switch (direct){
+            case 0:
+                
+            default:
+                System.out.println("...");
+        }
+
+
+
+
+    }
 }
