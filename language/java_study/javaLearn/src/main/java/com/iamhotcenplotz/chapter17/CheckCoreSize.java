f1502c8c94016e1fe43dc0dce2747b6d93012faa
Jiyu Guo <ddzsw2020@gmail.com>
2025-03-17 22:14:24 +0800

    a

---
+++ language/java_study/javaLearn/src/main/java/com/iamhotcenplotz/chapter17/CheckCoreSize.java
@@ -0,0 +1,13 @@
+package com.iamhotcenplotz.chapter17;
+
+/**
+ * @author Der Hotcenplotz
+ * @version 1.0
+ */
+public class CheckCoreSize {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println(runtime.availableProcessors());
+    }
+}
