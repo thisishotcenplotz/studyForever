1ad59029cbf301e5910c7914071398a8538edec2
Jiyu Guo <ddzsw2020@gmail.com>
2025-06-04 07:31:21 +0800

    a

--- language/framework_study/flink_java_study/FlinkJava1_17/src/main/java/com/hotcenplotz/chapter02/SocketWordCountStreamProcessDemo.java
+++ language/framework_study/flink_java_study/FlinkJava1_17/src/main/java/com/hotcenplotz/chapter02/SocketWordCountStreamProcessDemo.java
@@ -21,7 +21,7 @@ public class SocketWordCountStreamProcessDemo {
         StreamExecutionEnvironment flink = StreamExecutionEnvironment.getExecutionEnvironment();
         
         // TODO 2. 读取数据 socket
-        DataStreamSource<String> socketData = flink.socketTextStream("localhost", 10999);
+        DataStreamSource<String> socketData = flink.socketTextStream("hadoop101", 10999);
         
         // TODO 3. 处理数据: 切分 转tuple 聚合
         SingleOutputStreamOperator<Tuple2<String, Integer>> result = socketData.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
