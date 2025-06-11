08530cdd7e06efcdfd2ab71293a2a2a3047344b1
Jiyu Guo <ddzsw2020@gmail.com>
2025-06-11 23:52:12 +0800

    a

--- language/framework_study/flink_java_study/flinkstudy/src/main/java/com/hotcenplotz/study/chapter05/sink_/Demo03JDBCSink_.java
+++ language/framework_study/flink_java_study/flinkstudy/src/main/java/com/hotcenplotz/study/chapter05/sink_/Demo03JDBCSink_.java
@@ -3,29 +3,21 @@ package com.hotcenplotz.study.chapter05.sink_;
 import com.hotcenplotz.study.chapter05.bean.WaterSensor;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 
-import org.apache.flink.api.connector.sink2.Committer;
-import org.apache.flink.api.connector.sink2.SinkWriter;
-import org.apache.flink.connector.base.DeliveryGuarantee;
-import org.apache.flink.connector.jdbc.*;
-import org.apache.flink.connector.jdbc.core.datastream.Jdbc;
+
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
+import org.apache.flink.connector.jdbc.core.datastream.sink.JdbcSink;
 import org.apache.flink.connector.jdbc.core.datastream.sink.JdbcSinkBuilder;
-import org.apache.flink.connector.jdbc.core.datastream.sink.committer.JdbcCommitable;
-import org.apache.flink.connector.jdbc.datasource.connections.SimpleJdbcConnectionProvider;
+import org.apache.flink.core.execution.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.legacy.SinkFunction;
-import org.apache.flink.streaming.api.operators.collect.CollectSinkFunction;
+
 import org.apache.flink.util.Collector;
-import org.apache.flink.util.Preconditions;
 
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.Random;
 import java.util.UUID;
 
@@ -41,9 +33,9 @@ public class Demo03JDBCSink_ {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment flink = StreamExecutionEnvironment.getExecutionEnvironment();
         flink.setParallelism(3);
-
+        
         DataStreamSource<String> source = flink.socketTextStream("localhost", 7777);
-
+        
         SingleOutputStreamOperator<WaterSensor> waterSensor = source.flatMap(new FlatMapFunction<String, WaterSensor>() {
             @Override
             public void flatMap(String value, Collector<WaterSensor> out) throws Exception {
@@ -57,8 +49,41 @@ public class Demo03JDBCSink_ {
             }
         });
         
+        waterSensor.print();
+        
+        // 这才是flink2.0 写入mysql 的正确写法，官网的不对！！！
+        // https://nightlies.apache.org/flink/flink-docs-release-2.0/docs/connectors/datastream/jdbc/
+        JdbcSinkBuilder<WaterSensor> builder = JdbcSink.builder();
+        
+        JdbcSink<WaterSensor> waterSensorJdbcSink = builder.withQueryStatement(
+            "insert into flink.watersensor(id,ts,vc) values(?,?,?)",
+            new JdbcStatementBuilder<WaterSensor>() {
+                @Override
+                public void accept(PreparedStatement preparedStatement, WaterSensor waterSensor) throws SQLException {
+                    preparedStatement.setString(1, waterSensor.getId());
+                    preparedStatement.setLong(2, waterSensor.getTs());
+                    preparedStatement.setInt(3, waterSensor.getVc());
+                }
+            }
+        ).withExecutionOptions(
+            JdbcExecutionOptions.builder()
+                .withBatchIntervalMs(2000)
+                .withBatchSize(500)
+                .withMaxRetries(3)
+                .build()
+        ).buildAtLeastOnce(
+            new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                .withUrl("jdbc:mysql://localhost:3306/flink")
+                .withDriverName("com.mysql.cj.jdbc.Driver")
+                .withUsername("root")
+                .withPassword("123456")
+                .withConnectionCheckTimeoutSeconds(60)
+                .build()
+        );
+        
+        waterSensor.sinkTo(waterSensorJdbcSink);
 
 
-//        flink.execute();
+        flink.execute();
     }
 }
