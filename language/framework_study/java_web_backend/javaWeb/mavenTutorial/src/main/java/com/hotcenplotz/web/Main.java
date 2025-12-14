5ff468ef0266167cb08fc913a3e403cdf9cd6325
Jiyu Guo <ddzsw2020@gmail.com>
2025-12-14 08:26:58 +0800

    a

---
+++ language/framework_study/java_web_backend/javaWeb/mavenTutorial/src/main/java/com/hotcenplotz/web/Main.java
@@ -0,0 +1,15 @@
+package com.hotcenplotz.web;
+
+/**
+ * @author: Der Hotcenplotz
+ * @version： 1.0
+ * @date: 2025-12-13 18:11
+ * @description:
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
